550a642699102b853574eb9d11b9d8d99293a80f
marlonsilveiragomes <marlonsilveiragomesm>
2024-07-08 07:54:47 -0300

    atualizacao do escolha numero

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerossorteados = [];
-let numeroLimte = 100;
+let numeroLimte = 40;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -11,7 +11,7 @@ function exibirTextoNaTela(tag, texto) {
 function exibirMensagemInicial() {
     
 exibirTextoNaTela('h1', 'Jogo do número secreto');
-exibirTextoNaTela('p', 'Escolha um numero entre 1 e 100');
+exibirTextoNaTela('p', 'Escolha um numero entre 1 e 40');
 }
 
 exibirMensagemInicial();
